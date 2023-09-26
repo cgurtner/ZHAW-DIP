554cf10029efb52b36a5b59571f4d5a18a6c89d8
Cyrill Gurtner <me@cgurtner.ch>
2023-09-26 22:12:04 +0200

    completed Lab01

--- Lab01/run.py
+++ Lab01/run.py
@@ -1,5 +1,7 @@
 import os
+import cv2
 import skimage
+import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
@@ -56,4 +58,57 @@ converted_gray = skimage.color.rgb2gray(frame)
 print(converted_gray.shape) # (512, 512) ==> grayscale, 1 channel
 
 # plt.imshow(converted_gray)
+# plt.show()
+
+# 1.3
+folder = os.path.join(os.getcwd(), 'Lab01/pics/brain/')
+images = sorted(os.listdir(folder))
+
+## remove .DS_Store file if it exists because I'm working with macOS
+images = [img for img in images if img != '.DS_Store']
+
+slices = []
+
+for img in images:
+    path = os.path.join(folder, img)
+    img = skimage.io.imread(path)
+
+    ## 1.3.1 display individual images
+    # plt.imshow(img)
+    # plt.show()
+
+    slices.append(img)
+
+## 1.3.2 save sequenze as .gif
+output_file = 'brain_sequence.gif'
+pil_img = [Image.fromarray(slice) for slice in slices]
+pil_img[0].save(output_file, save_all=True, append_images=pil_img[1:], duration=100, loop=100)
+
+## 1.3.3 show single images in a stack
+stacked_singles = np.vstack(slices)
+# plt.imshow(stacked_singles)
+# plt.show()
+
+## 1.3.4 parallel to frontal plane
+stack = np.stack(slices, axis=2)
+
+s, M, N = stack.shape  # s is x-direction, M is y-direction, N is the number of slices
+center = s // 2  # center slice
+
+frontal = stack[center, :, :]
+
+dim_max = max(M, N)
+frontal_slice = np.flip(cv2.resize(frontal, (dim_max, dim_max), interpolation=cv2.INTER_LINEAR))
+
+# plt.imshow(frontal_slice)
+# plt.show()
+
+## 1.3.5 parallel to the sagittal plane
+center = M // 2 # re-center according to M
+
+sagittal = stack[:, center, :]
+max = max(sagittal.shape)
+
+sagittal_resize = np.flip(cv2.resize(sagittal, (max, max), interpolation=cv2.INTER_LINEAR))
+# plt.imshow(sagittal_resize)
 # plt.show()
